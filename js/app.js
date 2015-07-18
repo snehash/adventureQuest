a440f8ae1fd0c144a2d2c771b48e4782eb4feb2f
Connie Wang <connie.ru.wang@gmail.com>
2015-07-18 10:21:30 -0700

    resovle merge conflicts

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@
     /* Makes the chosen segment of the story appear and scrolls to the top of it.
      * Hides the buttons after the choice is made.
      */
-    function showNext(chosen) {
+    function showNext(chosen, hideOption) {
         // add to array
         // hide buttons
-        $(".option").hide()
+        hideOption.hide();
         chosen.show();
         $('html, body').animate({
             scrollTop: chosen.offset().top
@@ -33,143 +33,153 @@
 
         // intro
         $("button#bus-intro").on("click", function() {
-            showNext($(".bus-intro"));
+            showNext($(".bus-intro"), $("#walk-intro"));
         });
         $("button#walk-intro").on("click", function() {
-            showNext($(".walk-intro"));
+            showNext($(".walk-intro"), $("#bus-intro"));
         });
 
         // bus-intro
         $("button#bus-pluto").on("click", function() {
-            showNext($(".bus-pluto"));
+            showNext($(".bus-pluto"), $("#bus-alien"));
         });
         $("button#bus-alien").on("click", function() {
-            showNext($(".bus-alien"));
+            showNext($(".bus-alien"), $("#bus-pluto"));
         });
 
         // bus-alien
-        $("button.bus-build-button").on("click", function() {
-            showNext($(".bus-build"));
+        $("button.bus-amphitheater").on("click", function() {
+            showNext($(".bus-build"), $(".bus-garden"));
         });
+        $("button.bus-garden").on("click", function() {
+            showNext($(".bus-build"), $(".bus-amphitheater"));
+        });
+
         // bus-build
-        $("button#bus-right").on("click", function() {
-            showNext($(".bus-alien-final"));
+        $("button#bus-green").on("click", function() {
+            showNext($(".bus-alien-final"), $("#bus-blue"));
+        });
+        $("button#bus-blue").on("click", function() {
+            showNext($(".bus-alien-final"), $("#bus-green"));
         });
 
         // bus-pluto
         $("button#bus-contest").on("click", function() {
-            showNext($(".bus-contest"));
+            showNext($(".bus-contest"), $("#bus-telescope"));
         });
         $("button#bus-telescope").on("click", function() {
-            showNext($(".bus-telescope"));
+            showNext($(".bus-telescope"), $("#bus-contest"));
         });
 
         // bus-contest
         $("button#bus-judge").on("click", function() {
-            showNext($(".bus-judge"));
+            showNext($(".bus-judge"), $("#bus-vote"));
         });
         $("button#bus-vote").on("click", function() {
-            showNext($(".bus-vote"));
+            showNext($(".bus-vote"), $("#bus-judge"));
         });
 
         // bus-judge
         $("button#bus-judgeAgree").on("click", function() {
-            showNext($(".bus-noParticipate"));
+            showNext($(".bus-noParticipate"), $("#bus-judgeDisagree"));
         });
         $("button#bus-judgeDisagree").on("click", function() {
-            showNext($(".bus-participate"));
+            showNext($(".bus-participate"), $("#bus-judgeAgree"));
         });
 
         // bus-vote
         $("button#bus-voteAgree").on("click", function() {
-            showNext($(".bus-noParticipate"));
+            showNext($(".bus-voteDisagree"), $("#bus-voteDisagree"));
         });
         $("button#bus-voteDisagree").on("click", function() {
-            showNext($(".bus-participate"));
+            showNext($(".bus-voteAgree"), $("#bus-voteAgree"));
         });
 
         // bus-telescope
         $("button#bus-otherguy").on("click", function() {
-            showNext($(".bus-finishtele"));
+            showNext($(".bus-finishtele"), $("#bus-ownplans"));
         });
         $("button#bus-ownplans").on("click", function() {
-            showNext($(".bus-finishtele"));
+            showNext($(".bus-finishtele"), $("#bus-otherguy"));
         });
 
         // bus-finishtele
         $("button#bus-moretele").on("click", function() {
-            showNext($(".bus-moretele"));
+            showNext($(".bus-moretele"), $("#bus-onetele"));
         });
         $("button#bus-onetele").on("click", function() {
-            showNext($(".bus-onetele"));
+            showNext($(".bus-onetele"), $("#bus-moretele"));
         });
 
         // walk-intro
         $("button#walk-fireyourself").on("click", function() {
-            showNext($(".walk-done"));
+            showNext($(".walk-done"), $("#walk-fireelse"));
         });
         $("button#walk-fireelse").on("click", function() {
-            showNext($(".walk-done"));
+            showNext($(".walk-done"), $("#walk-fireyourself"));
         });
 
         // walk-done
         $("button#walk-elf").on("click", function() {
-            showNext($(".walk-elf"));
+            showNext($(".walk-elf"), $("#walk-dwarf"));
         });
         $("button#walk-dwarf").on("click", function() {
-            showNext($(".walk-dwarf"));
+            showNext($(".walk-dwarf"), $("#walk-elf"));
         });
 
         // walk-elf
         $("button#walk-stoptalk").on("click", function() {
-            showNext($(".walk-stoptalk"));
+            showNext($(".walk-stoptalk"), $("#walk-followelhir"));
         });
-        $("button#followelhir").on("click", function() {
-            showNext($(".followelhir"));
+        $("button#walk-followelhir").on("click", function() {
+            showNext($(".walk-followelhir"), $("#walk-stoptalk"));
         });
 
         // walk-stoptalk
         $("button#walk-voice").on("click", function() {
-            showNext($(".walk-voice"));
+            showNext($(".walk-voice"), $("#walk-flower"));
         });
         $("button#walk-flower").on("click", function() {
-            showNext($(".walk-flower"));
+            showNext($(".walk-flower"), $("#walk-voice"));
         });
 
         // walk-voice / walk-flower ugh the doubled ids could be problematic but i'm too tired to care rn i am sorry you guys
         $("button#walk-wander").on("click", function() {
-            showNext($(".walk-wander"));
+            showNext($(".walk-wander"), $("#walk-directions"));
         });
         $("button#walk-directions").on("click", function() {
-            showNext($(".walk-directions"));
+            showNext($(".walk-directions"), $("#walk-wander"));
         });
 
         // walk-followelhir
         $("button#walk-phonemusic").on("click", function() {
-            showNext($(".walk-phonemusic"));
+            showNext($(".walk-phonemusic"), $("#walk-ownmusic"));
         });
         $("button#walk-ownmusic").on("click", function() {
-            showNext($(".walk-ownmusic"));
+            showNext($(".walk-ownmusic"), $("#walk-phonemusic"));
         });
 
         // walk-dwarf
         $("button#walk-bridge").on("click", function() {
-            showNext($(".walk-bridge"));
+            showNext($(".walk-bridge"), $("#walk-physics"));
         });
         $("button#walk-physics").on("click", function() {
-            showNext($(".walk-physics"));
+            showNext($(".walk-physics"), $("#walk-bridge"));
         });
 
         // walk-bridge
         $("button#walk-baking").on("click", function() {
-            showNext($(".walk-baking"));
+            showNext($(".walk-baking"), $("#walk-sales"));
         });
         $("button#walk-sales").on("click", function() {
-            showNext($(".walk-sales"));
+            showNext($(".walk-sales"), $("#walk-baking"));
         });
 
+        // show careers
 
-
+        $("button#cp-3").on("click", function() {
+            showNext($(".cp-3"));
+        });
 
 
     });
